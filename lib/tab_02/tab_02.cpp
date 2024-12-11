e116fadc65b686d712c2bfdfbe613541c37735e7
moisesfa <moifdez@gmail.com>
2024-12-12 00:42:34 +0100

    cambio en diseño de datos en tab_02

--- lib/tab_02/tab_02.cpp
+++ lib/tab_02/tab_02.cpp
@@ -4,6 +4,7 @@
 #include "tabs_all.h"
 
 // Dirección para convrtir fuentes https://rop.nl/truetype2gfx/   
+#include "myfonts/AlibabaSans_Bold80pt7b.h"
 #include "myfonts/AlibabaSans_Bold40pt7b.h"
 #include "myfonts/AlibabaSans_Bold20pt7b.h"
 
@@ -44,16 +45,19 @@ void tab_02_view_data_ext(String msg_tem, String msg_hum)
     sprite_info_data_ext.setTextColor(TFT_LIGHTGRAY);
     sprite_info_data_ext.setTextDatum(CC_DATUM);
     //sprite_info_data_ext.drawString(msg_tem, WIDTH_SPRT_INFO_DATA_EXT / 2, HEIGHT_SPRT_INFO_DATA_EXT / 2 + 8);
-    sprite_info_data_ext.drawString("TEM", 50, HEIGHT_SPRT_INFO_DATA_EXT / 2 );
-    //sprite_info_data_ext.drawString("C", 230, HEIGHT_SPRT_INFO_DATA_EXT / 2 );
-    sprite_info_data_ext.drawString("HUM", 650, HEIGHT_SPRT_INFO_DATA_EXT / 2 );
-    sprite_info_data_ext.drawString("o", 260, HEIGHT_SPRT_INFO_DATA_EXT / 3 );
+    sprite_info_data_ext.drawString("TEMPERATURA", 175, 25 );
+    sprite_info_data_ext.drawString("o", 155, 80 );
+    sprite_info_data_ext.drawString("HUMEDAD", 525, 25 );
     sprite_info_data_ext.setFont(&AlibabaSans_Bold40pt7b);
-    sprite_info_data_ext.drawString("%", 550, HEIGHT_SPRT_INFO_DATA_EXT / 2 );
-    sprite_info_data_ext.drawString("C", 300, HEIGHT_SPRT_INFO_DATA_EXT / 2 );
-    sprite_info_data_ext.drawString(msg_tem, 180, HEIGHT_SPRT_INFO_DATA_EXT / 2 );
-    sprite_info_data_ext.drawString(msg_hum, 460, HEIGHT_SPRT_INFO_DATA_EXT / 2);
-    //sprite_info_data_ext.pushSprite(50, 100);
+    sprite_info_data_ext.drawString("C", 195, 100 );
+    sprite_info_data_ext.drawString("%", 525, 100 );
+    //sprite_info_data_ext.drawString("o", 260, HEIGHT_SPRT_INFO_DATA_EXT / 3 );
+    sprite_info_data_ext.setFont(&AlibabaSans_Bold80pt7b);
+    //sprite_info_data_ext.drawString("C", 300, HEIGHT_SPRT_INFO_DATA_EXT / 2 );
+    sprite_info_data_ext.drawString(msg_tem, 175, 240 );
+    sprite_info_data_ext.drawString(msg_hum, 525, 240);
+
+    //sprite_info_data_ext.pushSprite(50, 120);
     sprite_info_data_ext.pushSprite(50, 100,TFT_TRANSPARENT);
 
 }
--- lib/tab_02/tab_02.h
+++ lib/tab_02/tab_02.h
@@ -1,7 +1,7 @@
 #pragma once 
 
 #define WIDTH_SPRT_INFO_DATA_EXT    700
-#define HEIGHT_SPRT_INFO_DATA_EXT   100 
+#define HEIGHT_SPRT_INFO_DATA_EXT   300 
 
 void tab_02_view(void); 
 void tab_02_view_data_ext(String msg_tem, String msg_hum);
