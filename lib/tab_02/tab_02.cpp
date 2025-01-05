9c7d81268d0da6c2a5d9c208edf2134d721ffb19
moisesfa <moifdez@gmail.com>
2025-01-05 21:05:45 +0100

    añadir resultado final v0.3

--- lib/tab_02/tab_02.cpp
+++ lib/tab_02/tab_02.cpp
@@ -54,8 +54,8 @@ void tab_02_view_data_ext(String msg_tem, String msg_hum)
     //sprite_info_data_ext.drawString("o", 260, HEIGHT_SPRT_INFO_DATA_EXT / 3 );
     sprite_info_data_ext.setFont(&AlibabaSans_Bold80pt7b);
     //sprite_info_data_ext.drawString("C", 300, HEIGHT_SPRT_INFO_DATA_EXT / 2 );
-    sprite_info_data_ext.fillRoundRect(10, 159, 340, 150, 20, 0x000028);
-    sprite_info_data_ext.fillRoundRect(360, 159, 340, 150, 20, 0x000028);
+    sprite_info_data_ext.fillRoundRect(0, 158, 700, 150, 20, 0x000028);
+    //sprite_info_data_ext.fillRoundRect(360, 159, 340, 150, 20, 0x000028);
     
     sprite_info_data_ext.drawString(msg_tem, 175, 240 );
     sprite_info_data_ext.drawString(msg_hum, 525, 240);
